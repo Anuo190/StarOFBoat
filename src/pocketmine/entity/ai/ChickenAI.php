5c177b47f12f9b07680700f6f4804fc6d9d2c7f5
Anuo190 <125326994+Anuo190@users.noreply.github.com>
2024-03-12 22:52:10 +0800

    Add files via upload

--- src/pocketmine/entity/ai/ChickenAI.php
+++ src/pocketmine/entity/ai/ChickenAI.php
@@ -20,10 +20,7 @@ class ChickenAI
     public $width = 0.6;
     private $dif = 0;
 
-    /**
-     * 固定AITICK
-     * @param \pocketmine\entity\ai\AIHolder $AIHolder
-     */
+
     public function __construct(AIHolder $AIHolder)
     {
         $this->AIHolder = $AIHolder;
@@ -46,218 +43,176 @@ class ChickenAI
 
     public function ChickenRandomWalkCalc()
     {
-            //遍历存档
+        $this->dif = $this->AIHolder->getServer()->getDifficulty();
+        //$this->getLogger()->info("雞数量：".count($this->plugin->Chicken));
         foreach ($this->AIHolder->getServer()->getLevels() as $level) {
-
-            //获得难度
-            $this->dif = $this->AIHolder->getServer()->getDifficulty();
-
             foreach ($level->getEntities() as $zo) {
-                //判断存档里面有没有鸡
-
-                switch ($zo instanceof Chicken) {
-                    case 0:
-                        $this->AIHolder->Chicken[$zo->getId()] = array(
-                            'ID' => $zo->getId(),
-                            'IsChasing' => false,
-                            'motionx' => 0,
-                            'motiony' => 0,
-                            'motionz' => 0,
-                            'hurt' => 10,
-                            'time' => 10,
-                            'x' => 0,
-                            'y' => 0,
-                            'z' => 0,
-                            'oldv3' => $zo->getLocation(),
-                            'yup' => 20,
-                            'up' => 0,
-                            'yaw' => $zo->yaw,
-                            'pitch' => 0,
-                            'level' => $zo->getLevel()->getName(),
-                            'xxx' => 0,
-                            'zzz' => 0,
-                            'gotimer' => 10,
-                            'swim' => 0,
-                            'jump' => 0.01,
-                            'canjump' => true,
-                            'drop' => false,
-                            'canAttack' => 0,
-                            'knockBack' => false,
-                        );
-
-                }
-                //判断鸡周围的32格玩家
-                switch ($this->AIHolder->willMove($zo)) {
-
-                    case 0:
-                        $zom = &$this->AIHolder->Chicken[$zo->getId()];
-                        $zom['x'] = $zo->getX();
-                        $zom['y'] = $zo->getY();
-                        $zom['z'] = $zo->getZ();
-
-                    switch (!isset($this->AIHolder->Chicken[$zo->getId()])) {
-                        case 0:
+                if ($zo instanceof Chicken) {
+                    if ($this->AIHolder->willMove($zo)) {
+                        if (!isset($this->AIHolder->Chicken[$zo->getId()])) {
+                            $this->AIHolder->Chicken[$zo->getId()] = array(
+                                'ID' => $zo->getId(),
+                                'IsChasing' => false,
+                                'motionx' => 0,
+                                'motiony' => 0,
+                                'motionz' => 0,
+                                'hurt' => 10,
+                                'time' => 10,
+                                'x' => 0,
+                                'y' => 0,
+                                'z' => 0,
+                                'oldv3' => $zo->getLocation(),
+                                'yup' => 20,
+                                'up' => 0,
+                                'yaw' => $zo->yaw,
+                                'pitch' => 0,
+                                'level' => $zo->getLevel()->getName(),
+                                'xxx' => 0,
+                                'zzz' => 0,
+                                'gotimer' => 10,
+                                'swim' => 0,
+                                'jump' => 0.01,
+                                'canjump' => true,
+                                'drop' => false,
+                                'canAttack' => 0,
+                                'knockBack' => false,
+                            );
                             $zom = &$this->AIHolder->Chicken[$zo->getId()];
-
-                        switch ($zom['IsChasing'] === false) {
-                            case 0:
-                            if ($zom['gotimer'] == 0 or $zom['gotimer'] == 10) {
-                            //Calucator Move——计算移动
-                            for (
-                                $i = abs(mt_rand(-20, 20) / 10 -
-                                $zom['motionx']);
-                                $i >= mt_rand(-5, 5) / 10;
-                                ) { 
-                                $zom['motionx'] = $i;
-                            }
-
-                            for (
-                                $z = abs(mt_rand(-20, 20) / 10 -
-                                $zom['motionz']);
-                                $z >= mt_rand(-5, 5) / 10;
-                                ) {
-                                $zom['motionz'] = $z;
-                            }
-
-                            } elseif (
-                                $zom['gotimer'] >= 20 and
-                                $zom['gotimer'] <= 24
-                                ) {
-                                    $zom['motionx'] = mt_rand(-2, 2) / 10;
-                                    $zom['motionz'] = mt_rand(-2, 2) / 10;
-                                  }
-                                    $this->CantMove($zom, $zo);
-                                    $zom['gotimer'] += 0.2;
-                                    if ($zom['gotimer'] >= 22)
-                                        $zom['gotimer'] = 0.2;
-                                        $zom['yup'] = 0;
-                                        $zom['up'] = 0;
-
-                                }
-
+                            $zom['x'] = $zo->getX();
+                            $zom['y'] = $zo->getY();
+                            $zom['z'] = $zo->getZ();
                         }
+                        $zom = &$this->AIHolder->Chicken[$zo->getId()];
+
+                        if ($zom['IsChasing'] === false) {  //自由行走模式
+                            if ($zom['gotimer'] == 0 or $zom['gotimer'] == 10) {
+                                //限制转动幅度
+                                $newmx = mt_rand(-20, 20) / 10;
+                                while (abs($newmx - $zom['motionx']) >= mt_rand(-5, 5) / 10) {
+                                    $newmx = mt_rand(-20, 20) / 10;
+                                }
+                                $zom['motionx'] = $newmx;
+
+                                $newmz = mt_rand(-20, 20) / 10;
+                                while (abs($newmz - $zom['motionz']) >= mt_rand(-5, 5) / 10) {
+                                    $newmz = mt_rand(-20, 20) / 10;
+                                }
+                                $zom['motionz'] = $newmz;
+                            } elseif ($zom['gotimer'] >= 20 and $zom['gotimer'] <= 24) {
+                                $zom['motionx'] = mt_rand(-2, 2) / 10;
+                                $zom['motionz'] = mt_rand(-2, 2) / 10;
+
+                            }
+                            $zom['gotimer'] += 0.2;
+                            if ($zom['gotimer'] >= 22)
+                                $zom['gotimer'] = 0.2;
+
+                            //$zom['motionx'] = mt_rand(-10,10)/10;
+                            //$zom['motionz'] = mt_rand(-10,10)/10;
+                            $zom['yup'] = 0;
+                            $zom['up'] = 0;
+
+                            //boybook的y轴判断法
+                            //$width = $this->width;
+                            $pos = new Vector3($zom['x'] + $zom['motionx'], floor($zo->getY()) + 1, $zom['z'] + $zom['motionz']);  //目标坐标
+                            $zy = $this->AIHolder->ifjump($zo->getLevel(), $pos);
+                            if ($zy === false) {  //前方不可前进
+                                $pos2 = new Vector3($zom['x'], $zom['y'], $zom['z']);  //目标坐标
+                                if ($this->AIHolder->ifjump($zo->getLevel(), $pos2) === false) { //原坐标依然是悬空
+                                    $pos2 = new Vector3($zom['x'], $zom['y'] - 1, $zom['z']);  //下降
+                                    $zom['up'] = 1;
+                                    $zom['yup'] = 0;
+                                } else {
+                                    $zom['motionx'] = -$zom['motionx'];
+                                    $zom['motionz'] = -$zom['motionz'];
+                                    //转向180度，向身后走
+                                    $zom['up'] = 0;
+                                }
+                            } else {
+                                $pos2 = new Vector3($zom['x'] + $zom['motionx'], $zy - 1, $zom['z'] + $zom['motionz']);  //目标坐标
+                                if ($pos2->y - $zom['y'] < 0) {
+                                    $zom['up'] = 1;
+                                } else {
+                                    $zom['up'] = 0;
+                                }
+                            }
+
+                            if ($zom['motionx'] == 0 and $zom['motionz'] == 0) {  //雞停止
+                            } else {
+                                //转向计算
+                                $yaw = $this->AIHolder->getyaw($zom['motionx'], $zom['motionz']);
+                                //$zo->setRotation($yaw,0);
+                                $zom['yaw'] = $yaw;
+                                $zom['pitch'] = 0;
+                            }
+
+                            //更新坐标
+                            if (!$zom['knockBack']) {
+                                $zom['x'] = $pos2->getX();
+                                $zom['z'] = $pos2->getZ();
+                                $zom['y'] = $pos2->getY();
+                            }
+                            $zom['motiony'] = $pos2->getY() - $zo->getY();
+                            //echo($zo->getY()."\n");
+                            //var_dump($pos2);
+                            //var_dump($zom['motiony']);
+                            $zo->setPosition($pos2);
+                            //echo "SetPosition \n";
+                        }
+                    }
                 }
-
             }
         }
-        
-    }
-    /**
-     * 判断能不能移动
-     */
-    public function CantMove($zom,$zo)
-    {
-        $pos = new Vector3($zom['x'] + $zom['motionx'], 
-                           floor($zo->getY()) + 1, $zom['z'] + 
-                           $zom['motionz']);  
-        
-        //如果要跳
-        $zy = $this->AIHolder->ifjump($zo->getLevel(), $pos);
-
-        //如果不跳
-        switch ($zy === false) {  
-            case 0:
-            //计算pos2
-            $pos2 = new Vector3($zom['x'], $zom['y'], $zom['z']);
-            if ($this->AIHolder->ifjump($zo->getLevel(), $pos2) === false) { 
-                $pos2 = new Vector3($zom['y'] - 1); 
-                $zom['up'] = 1;
-                $zom['yup'] = 0;
-            } else {
-                $zom['motionx'] = -$zom['motionx'];
-                $zom['motionz'] = -$zom['motionz'];
-                $zom['up'] = 0;
-            }
-            case 1:
-            $pos2 = new Vector3($zom['x'] + $zom['motionx'], $zy - 1, $zom['z'] + $zom['motionz']);  //目标坐标
-            if ($pos2->y - $zom['y'] < 0) {
-                $zom['up'] = 1;
-            } else {
-                $zom['up'] = 0;
-            }
-        }
-
-        /**
-         * 判断motionx motionz
-         */
-        switch ($zom['motionx'] == 0 and $zom['motionz'] == 0) {
-            case 0:
-            return 0;
-            case 1:
-            $yaw = $this->AIHolder->getyaw($zom['motionx'], $zom['motionz']);
-            $zom['yaw'] = $yaw;
-            $zom['pitch'] = 0;
-        }
-
-        if (!$zom['knockBack']) {
-            $zom['x'] = $pos2->getX();
-            $zom['z'] = $pos2->getZ();
-            $zom['y'] = $pos2->getY();
-            $zom['motiony'] = $pos2->getY() - $zo->getY();
-        }
-        $zo->setPosition($pos2);
-       
     }
 
     public function ChickenRandomWalk()
     {
         foreach ($this->AIHolder->getServer()->getLevels() as $level) {
-            switch ($zo instanceof Chicken) {
-                case 0:
-                foreach ($level->getEntities() as $zo) {
-                    switch (isset($this->AIHolder->Chicken[$zo->getId()])) {
-                        case 0:
+            foreach ($level->getEntities() as $zo) {
+                if ($zo instanceof Chicken) {
+                    if (isset($this->AIHolder->Chicken[$zo->getId()])) {
                         $zom = &$this->AIHolder->Chicken[$zo->getId()];
-
                         if ($zom['canAttack'] != 0) {
                             $zom['canAttack'] -= 1;
                         }
+                        $pos = $zo->getLocation();
+                        //echo ($zom['IsChasing']."\n");
 
-                        case 1:
-                        //降落计算
+                        //真正的自由落体 by boybook
                         if ($zom['drop'] !== false) {
                             $olddrop = $zom['drop'];
-                            $drop = $zom['drop'];
                             $zom['drop'] += 0.5;
+                            $drop = $zom['drop'];
+                            //echo $drop."\n";
                             $dropy = $zo->getY() - ($olddrop * 0.05 + 0.0125);
-                            //阶梯计算
-                            $posd0 = new Vector3(floor($zo->getX()), floor($dropy), 
-                                                 floor($zo->getZ()));
+                            $posd0 = new Vector3(floor($zo->getX()), floor($dropy), floor($zo->getZ()));
                             $posd = new Vector3($zo->getX(), $dropy, $zo->getZ());
-                      
-                            switch($this->AIHolder->whatBlock($zo->getLevel(), $posd0)
-                                   == "air") {
-                                case 0:
-                                $zo->setPosition($posd);  
-                                case 1:
-                            //计算i值        
-                            for ($i = 1; $i <= $drop; $i += 1) {
-                                $posd0->y += 1;
-                                switch ($this->AIHolder->whatBlock($zo->getLevel(), 
-                                        $posd0) != "block") {
-                                    case 0:
-                                    $posd->y = $posd0->y;
-                                        
-                                    $h = $zom['drop'] * $zom['drop'] / 20;
-                                    $damage = $h - 3;
-                                        
-                                    if ($damage > 0) {
-                                        $zo->attack($damage, 
-                                        EntityDamageEvent::CAUSE_FALL);
-                                    }
-                                    $zom['drop'] = false;
-                                    break;
+                            if ($this->AIHolder->whatBlock($zo->getLevel(), $posd0) == "air") {
+                                $zo->setPosition($posd);  //下降
+                            } else {
+                                for ($i = 1; $i <= $drop; $i++) {
+                                    $posd0->y++;
+                                    if ($this->AIHolder->whatBlock($zo->getLevel(), $posd0) != "block") {
+                                        $posd->y = $posd0->y;
+                                        //$zo->setPosition($posd);  //下降完成
+                                        $h = $zom['drop'] * $zom['drop'] / 20;
+                                        $damage = $h - 3;
+                                        //echo($h . ": " . $damage . "\n");
+                                        if ($damage > 0) {
+                                            $zo->attack($damage, EntityDamageEvent::CAUSE_FALL);
+                                        }
+                                        $zom['drop'] = false;
+                                        break;
                                     }
                                 }
                             }
                         } else {
                             $drop = 0;
                         }
-                       
+                        //echo ".";
                         $pk3 = new SetEntityMotionPacket;
                         $pk3->entities = [
-                            [$zo->getID(), $zom['motionx'] / 10, 0, 
-                            $zom['motionz'] / 10]
+                            [$zo->getID(), $zom['motionx'] / 10, 0, $zom['motionz'] / 10]
                         ];
                         foreach ($zo->getViewers() as $pl) {
                             $pl->dataPacket($pk3);
@@ -268,20 +223,15 @@ class ChickenAI
             }
         }
     }
-    
-    /**
-     * 数组清除
-     * 用于清除生物
-     */
+
     public function array_clear()
     {
         if (count($this->AIHolder->Chicken) != 0) {
             foreach ($this->AIHolder->Chicken as $eid => $info) {
-                if (!($level->getEntity($eid) instanceof Entity)) {
                 foreach ($this->AIHolder->getServer()->getLevels() as $level) {
-                    
+                    if (!($level->getEntity($eid) instanceof Entity)) {
                         unset($this->AIHolder->Chicken[$eid]);
-                        
+                        //echo "清除 $eid \n";
                     }
                 }
             }
--- src/pocketmine/entity/ai/CowAI.php
+++ src/pocketmine/entity/ai/CowAI.php
@@ -21,12 +21,7 @@ class CowAI
 	public $width = 0.3;
 	private $dif = 0;
 
-	/**
-	 * 
-	 * 固定AITICK
-	 * @param \pocketmine\entity\ai\AIHolder $AIHolder
-	 * 
-	 */
+
 	public function __construct(AIHolder $AIHolder)
 	{
 		$this->AIHolder = $AIHolder;
@@ -45,36 +40,23 @@ class CowAI
 		}
 	}
 
-	/**
-	 * 计算牛的孙吉走路
-	 */
+
 	public function CowRandomWalkCalc()
 	{
-		//遍历存档
+		$this->dif = $this->AIHolder->getServer()->getDifficulty();
+
 		foreach ($this->AIHolder->getServer()->getLevels() as $level) {
-			//获得存档难度
-			$this->dif = $this->AIHolder->getServer()->getDifficulty();
-			//遍历世界生物类型
 			foreach ($level->getEntities() as $zo) {
-				$allowedEntities = [/*牛*/Cow::NETWORK_ID, 
-									/*蘑菇牛*/Mooshroom::NETWORK_ID, 
-									/*猪*/Pig::NETWORK_ID, 
-									/*羊*/Sheep::NETWORK_ID, 
-									/*流浪者*/Ocelot::NETWORK_ID, 
-									/*狼*/Wolf::NETWORK_ID];
-				
-				//遍历数组里是否有allowed entity里面的实体
-				switch (in_array($zo::NETWORK_ID, $allowedEntities)) {
-					case 0:
-						$zom = &$this->AIHolder->Cow[$zo->getId()];
-					switch($this->AIHolder->willMove($zo)) {
-						case 0:
+				$allowedEntities = [Cow::NETWORK_ID, Mooshroom::NETWORK_ID, Pig::NETWORK_ID, Sheep::NETWORK_ID, Ocelot::NETWORK_ID, Wolf::NETWORK_ID];
+
+				if (in_array($zo::NETWORK_ID, $allowedEntities)) {
+					if ($this->AIHolder->willMove($zo)) {
 						if (!isset($this->AIHolder->Cow[$zo->getId()])) {
 							$this->initializeCowData($zo);
-							$this->updateCowMovement($zom, $zo);
 						}
 
-						
+						$zom = &$this->AIHolder->Cow[$zo->getId()];
+						$this->updateCowMovement($zom, $zo);
 					}
 				}
 			}
@@ -83,7 +65,6 @@ class CowAI
 
 	private function initializeCowData($zo)
 	{
-		//牛的基本信息
 		$this->AIHolder->Cow[$zo->getId()] = [
 			'ID' => $zo->getId(),
 			'IsChasing' => false,
@@ -113,22 +94,22 @@ class CowAI
 		];
 	}
 
-	//更新牛顿运动
 	private function updateCowMovement(&$zom, $zo)
 	{
 		$zom = &$this->AIHolder->Cow[$zo->getId()];
 		if ($zom['gotimer'] == 0 or $zom['gotimer'] == 10) {
-			//计算运动
-			for ($i = abs(mt_rand(-20, 20) / 10 - $zom['motionx']); 
-				 $i >= mt_rand(-6, 6) / 10;) {
-				 $zom['motionx'] = $i;
+
+			$newmx = mt_rand(-20, 20) / 10;
+			while (abs($newmx - $zom['motionx']) >= mt_rand(-6, 6) / 10) {
+				$newmx = mt_rand(-20, 20) / 10;
 			}
-			
-			for ($z = abs(mt_rand(-20, 20) / 10 - $zom['motionz']);
-				 $z >= mt_rand(-6, 6) / 10;) {
-				 $zom['motionz'] = $z;
+			$zom['motionx'] = $newmx;
+
+			$newmz = mt_rand(-20, 20) / 10;
+			while (abs($newmz - $zom['motionz']) >= mt_rand(-6, 6) / 10) {
+				$newmz = mt_rand(-20, 20) / 10;
 			}
-			
+			$zom['motionz'] = $newmz;
 		} elseif ($zom['gotimer'] >= 20 and $zom['gotimer'] <= 24) {
 			$zom['motionx'] = mt_rand(-2, 2) / 10;
 			$zom['motionz'] = mt_rand(-2, 2) / 10;
@@ -140,20 +121,20 @@ class CowAI
 
 		$pos = new Vector3($zom['x'] + $zom['motionx'], floor($zo->getY()) + 1, $zom['z'] + $zom['motionz']);  //目标坐标
 		$zy = $this->AIHolder->ifjump($zo->getLevel(), $pos);
-		$pos2 = new Vector3($zom['x'], $zom['y'], $zom['z']);
 
-		switch ($zy === false) {
-			case 0:
+		if ($zy === false) {
+			$pos2 = new Vector3($zom['x'], $zom['y'], $zom['z']);
 			if ($this->AIHolder->ifjump($zo->getLevel(), $pos2) === false) {
+
 				$zom['yup'] = 0;
 			} else {
 
 				$zom['motionx'] = -$zom['motionx'];
 				$zom['motionz'] = -$zom['motionz'];
-				$zom['up'] = 0;
 
+				$zom['up'] = 0;
 			}
-			case 1:
+		} else {
 			$pos2 = new Vector3($zom['x'] + $zom['motionx'], $zy - 1, $zom['z'] + $zom['motionz']);
 			if ($pos2->y - $zom['y'] < 0) {
 				$zom['up'] = 1;
@@ -162,10 +143,9 @@ class CowAI
 			}
 		}
 
-		switch ($zom['motionx'] == 0 and $zom['motionz'] == 0) {
-			case 0:
-				return;
-			case 1:
+		if ($zom['motionx'] == 0 and $zom['motionz'] == 0) {
+		} else {
+
 			$yaw = $this->AIHolder->getyaw($zom['motionx'], $zom['motionz']);
 			$zom['yaw'] = $yaw;
 			$zom['pitch'] = 0;
@@ -187,31 +167,21 @@ class CowAI
 	{
 		foreach ($this->AIHolder->getServer()->getLevels() as $level) {
 			foreach ($level->getEntities() as $zo) {
-				$allowedEntities = [Cow::NETWORK_ID, 
-									Mooshroom::NETWORK_ID, 
-									Pig::NETWORK_ID, 
-									Sheep::NETWORK_ID, 
-									Ocelot::NETWORK_ID, 
-									Wolf::NETWORK_ID];
+				$allowedEntities = [Cow::NETWORK_ID, Mooshroom::NETWORK_ID, Pig::NETWORK_ID, Sheep::NETWORK_ID, Ocelot::NETWORK_ID, Wolf::NETWORK_ID];
 
-				switch(in_array($zo::NETWORK_ID, $allowedEntities)) {
-					case 0:
-					$zom = &$this->AIHolder->Cow[$zo->getId()];
-					switch (isset($this->AIHolder->Cow[$zo->getId()])) {
-						case 0:
+				if (in_array($zo::NETWORK_ID, $allowedEntities)) {
+					if (isset($this->AIHolder->Cow[$zo->getId()])) {
+						$zom = &$this->AIHolder->Cow[$zo->getId()];
 						if ($zom['canAttack'] != 0) {
 							$zom['canAttack'] -= 1;
 						}
 
 						$downly = $zo->onGround;
 
-						if (abs($zo->getY() - $zom['oldv3']->y) == 1 && 
-							$zom['canjump'] === true) 
-						{
+						if (abs($zo->getY() - $zom['oldv3']->y) == 1 && $zom['canjump'] === true) {
 							$zom['canjump'] = true;
 							$zom['jump'] = 0.1;
-						}
-						else {
+						} else {
 							if ($zom['jump'] > 0.01) {
 								$zom['jump'] -= 0.2;
 							} else {
@@ -221,10 +191,7 @@ class CowAI
 
 						$pk3 = new SetEntityMotionPacket;
 						$pk3->entities = [
-							[$zo->getID(), 
-							$zom['xxx'], 
-							$zom['jump'] - ($downly ? 0.04 : 0), 
-							$zom['zzz']]
+							[$zo->getID(), $zom['xxx'], $zom['jump'] - ($downly ? 0.04 : 0), $zom['zzz']]
 						];
 						foreach ($zo->getViewers() as $pl) {
 							$pl->dataPacket($pk3);
@@ -236,13 +203,12 @@ class CowAI
 	}
 	public function array_clear()
 	{
+		if (count($this->AIHolder->Cow) != 0) {
 			foreach ($this->AIHolder->Cow as $eid => $info) {
-			switch (count($this->AIHolder->Cow) != 0) {
-				case 0:
 				foreach ($this->AIHolder->getServer()->getLevels() as $level) {
 					if (!($level->getEntity($eid) instanceof Entity)) {
 						unset($this->AIHolder->Cow[$eid]);
-						
+						//echo "清除 $eid \n";
 					}
 				}
 			}
